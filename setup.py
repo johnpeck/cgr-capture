333513c241bd178ce44a98f6a62b0941090e8622
John Peck <sunnyvalejp@gmail.com>
2013-10-21 18:38:26 -0500

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'cgr-capture',
+    packages = ['cgr-capture'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Capture waveforms with the CGR-101 USB oscilloscope',
+    author = 'John Peck',
+    author_email = 'john@johnpeck.info',
+    url = 'https://github.com/johnpeck/cgr-capture',
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+    keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+    classifiers = [],
+)
