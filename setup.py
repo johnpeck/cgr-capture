9d41ada27fd9d8985854ed55855d2516c04c13e2
John Peck <john.peck@knowles.com>
2014-06-05 17:18:47 -0500

    Added pyserial as a dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ entry_points = {
 setup(
     name = 'cgrlib',
     packages = ['cgrlib','cgrlib.tools','cgrlib.test'],
-    version = '0.1.0',
+    version = '0.1.1',
     license='LICENSE.txt',
     description = 'Capture waveforms with the CGR-101 USB oscilloscope',
     author = 'John Peck',
@@ -22,7 +22,8 @@ setup(
         "numpy >= 1.7.1",
         "configobj >= 4.7.2",
         "gnuplot-py >= 1.8",
-        "colorlog >= 2.0.0"
+        "colorlog >= 2.0.0",
+        "pyserial >= 2.6"
     ],
     zip_safe = True,
     entry_points = entry_points,
